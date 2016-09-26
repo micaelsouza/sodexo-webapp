e35ec3ee1c15fc6cf427656e3b06e85ba2d61eac
Micael Souza <micael.souza@outlook.com>
2016-09-26 17:54:11 -0300

    Grunt watch + Livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,28 @@
 module.exports = (grunt) => {
 
-  let concat = {
-    css: {
-      src: ['src/css/*.css'],
-      dest: 'dist/css/<%= pkg.name %>.min.css'
-    },
-    js: {
-      src: ['src/js/*.js'],
-      dest: 'dist/js/<%= pkg.name %>.min.js'
-    }
-  };
-
-  let watch = {
-    files: ['src/css/*.css', 'src/js/*.js'],
-    tasks: ['concat']
-  };
-
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: concat,
-    watch: watch
+    watch: {
+      files: ['*.css', '*.js', './*.html'],
+      options: {
+        livereload: true,
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          hostname: 'localhost',
+          base: '.',
+          livereload: true,
+          open: true
+        }
+      }
+    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['watch', 'concat']);
+  grunt.registerTask('default', ['connect', 'watch']);
 
 };
