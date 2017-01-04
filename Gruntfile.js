9d2f21dada1733db8ada3b60469b65216471efd7
micaelsouza <micael.souza@outlook.com>
2017-01-04 17:02:46 -0300

    Grunt tasks dev & build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,59 @@
 module.exports = (grunt) => {
 
+  const srcJSFiles = [
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/angular/angular.min.js',
+    'src/js/app.js',
+    'src/js/app.cards.controller.js',
+    'src/js/app.cardsAPI.service.js',
+    'src/js/app.idGenerator.service.js'
+  ];
+
+  const srcCSSFiles = [
+    'src/css/app.css',
+    'src/css/app.cards.css',
+    'src/css/app.addCards.css'
+  ];
+
   grunt.initConfig({
-    watch: {
-      files: ['src/**/*.css', 'src/**/*.js', './*.html'],
-      options: {
-        livereload: true,
+    copy: {
+      imgs: {
+        files: [{
+          expand: true,
+          cwd: 'src/images',
+          src: ['*'],
+          dest: 'dist/images'
+        }]
       }
     },
+    uglify: {
+      options: {mangle: false},
+      build: {files: {'dist/app.min.js': srcJSFiles}}
+    },
+    cssmin: {build: {files: {'dist/app.min.css': srcCSSFiles}}},
+    concat: {
+      js: {src: srcJSFiles , dest: 'dist/app.min.js'},
+      css: {src: srcCSSFiles, dest: 'dist/app.min.css'}
+    },
+    watch: {
+      js: {files: srcJSFiles, tasks: ['concat:js']},
+      css: {files: srcCSSFiles, tasks: ['concat:css'], options: {livereload: true}},
+      html: {files: './*html'},
+      options: {livereload: true}
+    },
     connect: {
-      server: {
-        options: {
-          port: 9001,
-          hostname: 'localhost',
-          base: '.',
-          livereload: true,
-          open: true
-        }
-      }
+      server: {options: {port: 9001, hostname: 'localhost', livereload: true}}
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('dev', ['copy', 'concat', 'connect', 'watch']);
+  grunt.registerTask('build', ['copy', 'uglify', 'cssmin']);
 
 };
